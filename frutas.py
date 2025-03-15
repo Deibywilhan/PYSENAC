9304adbfb82086326a19578b630e3a5431757d8f
Deibywilhan <157854956+Deibywilhan@users.noreply.github.com>
2025-03-14 21:42:22 -0300

    Aula dia 14

---
+++ frutas.py
@@ -0,0 +1,37 @@
+#frutas=["morango","maça","banana","cereja"]
+#for item in frutas:
+  #  print(f"\t{item.upper()}")
+   # input("\nPRESSIONE ENTER\n")
+
+#contagem =(9,8,7,6,5,4,3,2,1,0)
+#for item in contagem:
+  #  print(f"{item}...")
+
+#for i in range(1000):
+  #  print(i)
+#for i in range(3): 
+   # print(f"{i}-{frutas[i]}")
+#contador =0
+#while contador<5:
+   # print(contador)
+   # contador+=1
+##matriz = [
+   # [1,2,3],
+   # [4,5,6],
+  #  [7,8,9]
+#]
+#for linha in matriz:
+    #for elemento in linha:
+      #  print(elemento,end="")
+    #print()
+#for i in range(53):
+  #  if i ==53:
+   #     break 
+   # if i % 2==0:
+   #     continue
+   # print(i)
+for i in range(3):
+    print(i)
+else:
+    print("Loop concluido!")
+    fazer o desafio 6
